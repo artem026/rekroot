85f19558786941ff9866dd5898c759f244bd64ed
Artem026 <kris_my@mail.ru>
2023-10-28 23:45:52 +0300

    new commit 2023_10_28_10

--- HelloDjango/myapp1/views.py
+++ HelloDjango/myapp1/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index_page(request):
-    return render(request, 'index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'index.html', data)
 
 def about_page(request):
     return render(request, 'about.html')
