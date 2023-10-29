0f40418bf4db9f980fc976afe0ca14c2603b417a
Artem026 <kris_my@mail.ru>
2023-10-29 13:34:48 +0300

    new commit 2023_10_29_01

--- HelloDjango/HelloDjango/settings.py
+++ HelloDjango/HelloDjango/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp1'
+    'myapp1',
+    'news'
 ]
 
 MIDDLEWARE = [
--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp1.views import index_page, about_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_page, name='home'),
-    path('about/', about_page, name='about')
+    path('about/', about_page, name='about'),
+    path('news/', include('news.urls'))
 ]
