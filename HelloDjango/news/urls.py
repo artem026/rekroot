e2c6b722d78c4db6f01928e14a5faf4805b316d3
Artem026 <kris_my@mail.ru>
2023-10-31 18:55:05 +0300

    new commit 2023_10_31_14

--- HelloDjango/news/urls.py
+++ HelloDjango/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.news_create, name='news_create')
+    path('create', views.news_create, name='news_create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_details')
 ]
--- HelloDjango/news/views.py
+++ HelloDjango/news/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from . models import Articles
 from . forms import ArticlesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news_details.html'
+    context_object_name = 'article'
 
 def news_create(request):
     error = ''
