4893e974025690065787fae98ff7b50b94571264
Artem026 <kris_my@mail.ru>
2023-10-31 13:36:35 +0300

    new commit 2023_10_31_02

--- HelloDjango/news/views.py
+++ HelloDjango/news/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from . models import Articles
+from . forms import ArticlesForm
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news_home.html', {'news': news})
 
 def news_create(request):
-    return render(request, 'news_create.html')
+    form = ArticlesForm()
+    data = {'form': form}
+    return render(request, 'news_create.html', data)
