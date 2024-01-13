bc6c68966688c85f7f8add6b38e663834dd83ff1
Artem026 <kris_my@mail.ru>
2024-01-13 17:36:14 +0300

    commit 2024_01_13_01

--- HelloDjango/news/models.py
+++ HelloDjango/news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
-    anons = models.CharField('Анонс', max_length=250)
+    anons = models.CharField('Компания', max_length=250)
     full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
     salary = models.CharField('Зарплата', max_length=50)
